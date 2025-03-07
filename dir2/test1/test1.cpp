f3349327f345feeb8bead78906bb1b3ecfdbc55d
dz6(1) <vladyuzevuts@gmail.com>
2025-03-07 20:33:16 +0200

    add class

--- dir2/test1/test1.cpp
+++ dir2/test1/test1.cpp
@@ -2,7 +2,22 @@
 #include <iostream>
 
 using namespace std;
+class ITSTEP
+{
+public:
+	ITSTEP();
+	~ITSTEP();
 
+
+};
+
+ITSTEP::ITSTEP()
+{
+}
+
+ITSTEP::~ITSTEP()
+{
+}
 int main()
 {
     cout << "sadada";
