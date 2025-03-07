7bcbc0b1a4a4b5017e6763c874f55d8ce3755499
Ivalexz <daceeru@gmail.com>
2025-03-07 20:39:29 +0200

    Add my_sum

--- dir2/test1/test1.cpp
+++ dir2/test1/test1.cpp
@@ -13,10 +13,14 @@ public:
 
 ITSTEP::ITSTEP()
 {
-}
+};
 
 ITSTEP::~ITSTEP()
 {
+};
+
+void my_sum(int a) {
+	cout << "Sum: "<<7 + a << endl;
 }
 int main()
 {
@@ -29,4 +33,7 @@ int main()
 	cout << "hello";
 
 	cout << "Hello IT Step! " << endl;
+
+
+	my_sum(4);
 }
